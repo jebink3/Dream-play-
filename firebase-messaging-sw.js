f78e960acbce8cbfd4c4db9b4941aeb7695bb338
jebink3 <Jecobjebin@gmail.com>
2026-09-21 22:42:52 +0530

    Update firebase-messaging-sw.js

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,5 +1,5 @@
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-app-compat.js");
-importScripts("https://www.gstatic.com/firebasejs/10.12.2/firebase-messaging-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/11.0.2/firebase-app-compat.js");
+importScripts("https://www.gstatic.com/firebasejs/11.0.2/firebase-messaging-compat.js");
 
 firebase.initializeApp({
   apiKey: "AIzaSyDNJpHm2zUExVeSumw3IJAtcpaY-VFa5Ks",
